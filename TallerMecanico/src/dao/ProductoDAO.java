25dd563776b460fc348723d1b6d3ef5334a8a6e6
MatiasManzano <matias.manzano89@gmail.com>
2019-09-29 14:53:31 -0300

    Se agregan excepciones

--- TallerMecanico/src/dao/ProductoDAO.java
+++ TallerMecanico/src/dao/ProductoDAO.java
@@ -3,12 +3,13 @@ package dao;
 import java.sql.SQLException;
 
 import entidades.Producto;
+import excepciones.NoIdObtainedException;
 
 public interface ProductoDAO {
 
 	public Producto selectProducto(int codigoProducto) throws SQLException;
 
-	public void insertProducto(Producto producto) throws SQLException;
+	public void insertProducto(Producto producto) throws SQLException, NoIdObtainedException;
 
 	public void updateProducto(Producto producto) throws SQLException;
 
