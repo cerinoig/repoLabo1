2d04bbd7df4d7c342f8d9702d318e0f793c1d11a
MatiasManzano <matias.manzano89@gmail.com>
2019-09-29 14:56:06 -0300

    Se modifican los metodos para que lancen bien las excepciones y se borra el metodo buascar

--- TallerMecanico/src/dao/UsuarioDAO.java
+++ TallerMecanico/src/dao/UsuarioDAO.java
@@ -3,17 +3,18 @@ package dao;
 import java.sql.SQLException;
 
 import entidades.Usuario;
+import excepciones.ExistingUserException;
+import excepciones.NoIdObtainedException;
+import excepciones.NonExistingUserException;
 
 public interface UsuarioDAO {
 
-	public Usuario selectUsuario(String usuario) throws SQLException;
+	public Usuario selectUsuario(String usuario) throws SQLException, NonExistingUserException;
 
-	public void insertUsuario(Usuario usuario) throws SQLException;
+	public void insertUsuario(Usuario usuario) throws SQLException, NoIdObtainedException, ExistingUserException;
 
-	public void updateUsuario(Usuario usuario) throws SQLException;
+	public void updateUsuario(Usuario usuario) throws SQLException, NonExistingUserException;
 
-	public void deleteUsuario(String usuario) throws SQLException;
-	
-	public boolean buscarUsuario(String usuario) throws SQLException;
+	public void deleteUsuario(String usuario) throws SQLException, NonExistingUserException;
 
 }
--- TallerMecanico/src/dao/UsuarioDAOImpl.java
+++ TallerMecanico/src/dao/UsuarioDAOImpl.java
@@ -4,8 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import entidades.Usuario;
+import excepciones.ExistingUserException;
+import excepciones.NoIdObtainedException;
+import excepciones.NonExistingUserException;
 import utils.ConnectionManager;
 
 public class UsuarioDAOImpl implements UsuarioDAO {
@@ -16,17 +20,19 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 	private String sql;
 
 	@Override
-	public Usuario selectUsuario(String nombreUsuario) throws SQLException {
+	public Usuario selectUsuario(String nombreUsuario) throws SQLException, NonExistingUserException {
 		Usuario usuario = new Usuario();
 		try {
 			sql = "SELECT * FROM USUARIOS WHERE USUARIO = " + "'" + nombreUsuario + "'";
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 
-			while (rs.next()) {
+			if (rs.next()) {
 				usuario.setIdUsuario(rs.getInt("ID_USUARIO"));
 				usuario.setUsuario(rs.getString("USUARIO"));
 				usuario.setPassword(rs.getString("PASSWORD"));
+			} else {
+				throw new NonExistingUserException();
 			}
 		} catch (SQLException sqle) {
 			try {
@@ -48,23 +54,49 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 	}
 
 	@Override
-	public void insertUsuario(Usuario usuario) throws SQLException {
+	public void insertUsuario(Usuario usuario) throws SQLException, NoIdObtainedException, ExistingUserException {
 		try {
-			sql = "INSERT INTO USUARIOS (USUARIO, PASSWORD) VALUES (?, ?)";
+			sql = "SELECT * FROM USUARIOS WHERE USUARIO = " + "'" + usuario.getUsuario() + "'";
 			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
 
-			pstmt.setString(1, usuario.getUsuario());
-			pstmt.setString(2, usuario.getPassword());
+			if (rs.next()) {
+				throw new ExistingUserException();
+			} else {
+				try {
+					sql = "INSERT INTO USUARIOS (USUARIO, PASSWORD) VALUES (?, ?)";
+					pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-			pstmt.executeUpdate();
-			conn.commit();
+					pstmt.setString(1, usuario.getUsuario());
+					pstmt.setString(2, usuario.getPassword());
 
-		} catch (SQLException sqle) {
-			try {
-				conn.rollback();
-				sqle.printStackTrace();
-			} catch (SQLException sqle2) {
-				sqle2.printStackTrace();
+					pstmt.executeUpdate();
+					conn.commit();
+
+					try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+						if (generatedKeys.next()) {
+							usuario.setIdUsuario(generatedKeys.getInt(1));
+						} else {
+							throw new NoIdObtainedException();
+						}
+					}
+
+				} catch (SQLException sqle) {
+					try {
+						conn.rollback();
+						sqle.printStackTrace();
+					} catch (SQLException sqle2) {
+						sqle2.printStackTrace();
+					}
+				} finally {
+					try {
+						if (conn != null)
+							conn.close();
+					} catch (SQLException sqle3) {
+						sqle3.printStackTrace();
+					}
+				}
+
 			}
 		} finally {
 			try {
@@ -78,22 +110,39 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 	}
 
 	@Override
-	public void updateUsuario(Usuario usuario) throws SQLException {
+	public void updateUsuario(Usuario usuario) throws SQLException, NonExistingUserException {
 		try {
-			sql = "UPDATE USUARIOS SET USUARIO = ?, PASSWORD = ?";
+			sql = "SELECT * FROM USUARIOS WHERE USUARIO = " + "'" + usuario.getUsuario() + "'";
 			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
 
-			pstmt.setString(1, usuario.getUsuario());
-			pstmt.setString(2, usuario.getPassword());
+			if (rs.next()) {
+				try {
+					sql = "UPDATE USUARIOS SET USUARIO = ?, PASSWORD = ?";
+					pstmt = conn.prepareStatement(sql);
 
-			pstmt.executeUpdate();
+					pstmt.setString(1, usuario.getUsuario());
+					pstmt.setString(2, usuario.getPassword());
 
-		} catch (SQLException sqle) {
-			try {
-				conn.rollback();
-				sqle.printStackTrace();
-			} catch (SQLException sqle2) {
-				sqle2.printStackTrace();
+					pstmt.executeUpdate();
+
+				} catch (SQLException sqle) {
+					try {
+						conn.rollback();
+						sqle.printStackTrace();
+					} catch (SQLException sqle2) {
+						sqle2.printStackTrace();
+					}
+				} finally {
+					try {
+						if (conn != null)
+							conn.close();
+					} catch (SQLException sqle3) {
+						sqle3.printStackTrace();
+					}
+				}
+			} else {
+				throw new NonExistingUserException();
 			}
 		} finally {
 			try {
@@ -106,48 +155,45 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 	}
 
 	@Override
-	public void deleteUsuario(String usuario) throws SQLException {
-		try {
-			sql = "DELETE FROM USUARIOS WHERE USUARIO = ?";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, usuario);
-			pstmt.executeUpdate();
-
-		} catch (SQLException sqle) {
-			try {
-				conn.rollback();
-				sqle.printStackTrace();
-			} catch (SQLException sqle2) {
-				sqle2.printStackTrace();
-			}
-		} finally {
-			try {
-				if (conn != null)
-					conn.close();
-			} catch (SQLException sqle3) {
-				sqle3.printStackTrace();
-			}
-		}
-	}
-
-	@Override
-	public boolean buscarUsuario(String usuario) throws SQLException {
+	public void deleteUsuario(String usuario) throws SQLException, NonExistingUserException {
 		try {
 			sql = "SELECT * FROM USUARIOS WHERE USUARIO = " + "'" + usuario + "'";
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 
 			if (rs.next()) {
-				return true;
+				try {
+					sql = "DELETE FROM USUARIOS WHERE USUARIO = ?";
+					pstmt = conn.prepareStatement(sql);
+
+					pstmt.setString(1, usuario);
+					pstmt.executeUpdate();
+
+				} catch (SQLException sqle) {
+					try {
+						conn.rollback();
+						sqle.printStackTrace();
+					} catch (SQLException sqle2) {
+						sqle2.printStackTrace();
+					}
+				} finally {
+					try {
+						if (conn != null)
+							conn.close();
+					} catch (SQLException sqle3) {
+						sqle3.printStackTrace();
+					}
+				}
+			} else {
+				throw new NonExistingUserException();
 			}
-
-		} catch (SQLException se) {
-			se.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException sqle3) {
+				sqle3.printStackTrace();
+			}
 		}
-
-		return false;
 	}
 }
