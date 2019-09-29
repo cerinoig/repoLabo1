847a7253147be065e75326460368f0234a92ac2e
MatiasManzano <matias.manzano89@gmail.com>
2019-09-29 14:57:08 -0300

    Se crea excepcion para el caso en que no encuentre el ID en la base

---
+++ TallerMecanico/src/excepciones/NoIdObtainedException.java
@@ -0,0 +1,23 @@
+package excepciones;
+
+public class NoIdObtainedException extends Exception{
+	
+private static final long serialVersionUID = 1L;
+	
+	public NoIdObtainedException() {
+		super();
+	}
+	
+	public NoIdObtainedException(Throwable t) {
+		super();
+	}
+	
+	public NoIdObtainedException(Throwable t, String message) {
+		super();
+	}
+	
+	public NoIdObtainedException(String message) {
+		super();
+	}
+
+}
