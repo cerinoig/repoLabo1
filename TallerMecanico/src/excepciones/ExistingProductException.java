370b8e8b84f33d565d13de39f5cece6ad6abb63c
MatiasManzano <matias.manzano89@gmail.com>
2019-10-01 12:24:21 -0300

    Se agregan excepciones nuevas

---
+++ TallerMecanico/src/excepciones/ExistingProductException.java
@@ -0,0 +1,23 @@
+package excepciones;
+
+public class ExistingProductException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ExistingProductException() {
+		super();
+	}
+	
+	public ExistingProductException(Throwable t) {
+		super();
+	}
+	
+	public ExistingProductException(Throwable t, String message) {
+		super();
+	}
+	
+	public ExistingProductException(String message) {
+		super();
+	}
+
+}
