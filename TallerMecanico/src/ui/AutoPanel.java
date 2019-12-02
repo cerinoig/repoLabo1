65a50bab6039038ba6c7601c4236e3bbd28580cd
MatiasManzano <matias.manzano89@gmail.com>
2019-12-02 20:17:28 -0300

    Se traen a esta clase dos metodos que se repetian en Eliminar y en Modificar

--- TallerMecanico/src/ui/AutoPanel.java
+++ TallerMecanico/src/ui/AutoPanel.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.awt.Color;
+
 import javax.swing.JTextField;
 
 import handler.Handler;
@@ -20,6 +22,34 @@ public abstract class AutoPanel extends MiPanel {
 		super(handler);
 	}
 
-	//public abstract void initPanel(Handler handler);
+	@Override
+	public void deshabilitarCampos() {
+		patenteTextField.setEditable(false);
+		patenteTextField.setBackground(Color.LIGHT_GRAY);
+		marcaTextField.setEditable(false);
+		marcaTextField.setBackground(Color.LIGHT_GRAY);
+		modeloTextField.setEditable(false);
+		modeloTextField.setBackground(Color.LIGHT_GRAY);
+		colorTextField.setEditable(false);
+		colorTextField.setBackground(Color.LIGHT_GRAY);
+		puertasTextField.setEditable(false);
+		puertasTextField.setBackground(Color.LIGHT_GRAY);
+		kilometrosTextField.setEditable(false);
+		kilometrosTextField.setBackground(Color.LIGHT_GRAY);
+		añoTextField.setEditable(false);
+		añoTextField.setBackground(Color.LIGHT_GRAY);
+	}
+
+	@Override
+	public void limpiarCampos() {
+		patenteTextField.setText("");
+		marcaTextField.setText("");
+		modeloTextField.setText("");
+		colorTextField.setText("");
+		puertasTextField.setText("");
+		kilometrosTextField.setText("");
+		añoTextField.setText("");
+		buscarTextfield.setText("");
+	}
 
 }
