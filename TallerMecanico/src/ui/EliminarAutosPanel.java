e1d14dd50ccc1a9a90cdd979cd066fa93a5dd99c
MatiasManzano <matias.manzano89@gmail.com>
2019-12-02 20:21:15 -0300

    Se sacan los metodos que se repetian en ModificarAuto y se pasan al AutoPanel y se agregan los metodos para accionBuscar y accionConfirmar en las acciones de los botones

--- TallerMecanico/src/ui/EliminarAutosPanel.java
+++ TallerMecanico/src/ui/EliminarAutosPanel.java
@@ -1,6 +1,5 @@
 package ui;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -26,56 +25,25 @@ public class EliminarAutosPanel extends AutoPanel {
 		buscarBoton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
-				if (handler.consultaAuto(buscarTextfield.getText()) != null) {
-					objectToPanel(handler.consultaAuto(buscarTextfield.getText().toUpperCase()));
-					confirmarBoton.setEnabled(true);
-				}
+				accionBuscar(handler);
 			}
 		});
 
 		confirmarBoton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				handler.deleteAuto(patenteTextField.getText().toUpperCase());
-				limpiarCampos();
+				accionConfirmar(handler);
 			}
 		});
-
-		cancelarBoton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				handler.irAlInicio();
-			}
-		});
-	}
-
-	public void deshabilitarCampos() {
-		patenteTextField.setEditable(false);
-		patenteTextField.setBackground(Color.LIGHT_GRAY);
-		marcaTextField.setEditable(false);
-		marcaTextField.setBackground(Color.LIGHT_GRAY);
-		modeloTextField.setEditable(false);
-		modeloTextField.setBackground(Color.LIGHT_GRAY);
-		colorTextField.setEditable(false);
-		colorTextField.setBackground(Color.LIGHT_GRAY);
-		puertasTextField.setEditable(false);
-		puertasTextField.setBackground(Color.LIGHT_GRAY);
-		kilometrosTextField.setEditable(false);
-		kilometrosTextField.setBackground(Color.LIGHT_GRAY);
-		añoTextField.setEditable(false);
-		añoTextField.setBackground(Color.LIGHT_GRAY);
 	}
 
 	@Override
 	public Object panelToObject() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void objectToPanel(Object object) {
-
 		patenteTextField.setText(((Auto) object).getPatente());
 		marcaTextField.setText(((Auto) object).getMarca());
 		modeloTextField.setText(((Auto) object).getModelo());
@@ -83,18 +51,6 @@ public class EliminarAutosPanel extends AutoPanel {
 		puertasTextField.setText(String.valueOf(((Auto) object).getCantidadPuertas()));
 		kilometrosTextField.setText(String.valueOf(((Auto) object).getKilometraje()));
 		añoTextField.setText(((Auto) object).getAño());
-
-	}
-
-	@Override
-	public void limpiarCampos() {
-		patenteTextField.setText("");
-		marcaTextField.setText("");
-		modeloTextField.setText("");
-		colorTextField.setText("");
-		puertasTextField.setText("");
-		kilometrosTextField.setText("");
-		añoTextField.setText("");
 	}
 
 	@Override
@@ -104,7 +60,6 @@ public class EliminarAutosPanel extends AutoPanel {
 
 	@Override
 	public Box getBody() {
-
 		initTextFields();
 		deshabilitarCampos();
 		String[] labels = { "Buscar auto", "Patente", "Marca", "Modelo", "Color", "Cantidad de Puertas", "Kilometros",
@@ -117,18 +72,15 @@ public class EliminarAutosPanel extends AutoPanel {
 
 	@Override
 	public JButton[] getBotones() {
-
 		initButtons();
 		confirmarBoton.setEnabled(false);
 		JButton[] botones = { buscarBoton, confirmarBoton, cancelarBoton };
 
 		return botones;
-
 	}
 
 	@Override
 	public void initTextFields() {
-
 		buscarTextfield = new JTextField();
 		patenteTextField = new JTextField();
 		marcaTextField = new JTextField();
@@ -137,15 +89,30 @@ public class EliminarAutosPanel extends AutoPanel {
 		puertasTextField = new JTextField();
 		kilometrosTextField = new JTextField();
 		añoTextField = new JTextField();
-
 	}
 
 	@Override
 	public void initButtons() {
-
 		buscarBoton = new JButton("Eliminar");
 		confirmarBoton = new JButton("Confirmar");
 		cancelarBoton = new JButton("Cancelar");
+	}
 
+	@Override
+	public void accionConfirmar(Handler handler) {
+		handler.deleteAuto(patenteTextField.getText().toUpperCase());
+		limpiarCampos();
+	}
+
+	@Override
+	public void accionBuscar(Handler handler) {
+		if (handler.consultaAuto(buscarTextfield.getText()) != null) {
+			objectToPanel(handler.consultaAuto(buscarTextfield.getText().toUpperCase()));
+			confirmarBoton.setEnabled(true);
+		}
+	}
+
+	@Override
+	public void habilitarCampos() {
 	}
 }
