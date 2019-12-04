3929b0ee6f537ec506bf4056a50a483aa169fd24
MatiasManzano <matias.manzano89@gmail.com>
2019-12-03 22:33:24 -0300

    Se agrega el campo costo del arreglo

--- TallerMecanico/src/entidades/Factura.java
+++ TallerMecanico/src/entidades/Factura.java
@@ -6,38 +6,46 @@ public class Factura {
 	private int idFactura;
 	private String patente;
 	private String arreglo;
+	private Double costoAreglo;
 	private boolean cobrado;
 
 	public int getIdFactura() {
 		return idFactura;
 	}
 
-	public void setIdFactura(int idFactura) {
-		this.idFactura = idFactura;
-	}
-	
-	public String getArreglo() {
-		return arreglo;
-	}
-
-	public void setArreglo(String arreglo) {
-		this.arreglo = arreglo;
-	}
-
 	public String getPatente() {
 		return patente;
 	}
 
-	public void setPatente(String patente) {
-		this.patente = patente;
+	public String getArreglo() {
+		return arreglo;
+	}
+
+	public Double getCostoAreglo() {
+		return costoAreglo;
 	}
 
 	public boolean isCobrado() {
 		return cobrado;
 	}
 
+	public void setIdFactura(int idFactura) {
+		this.idFactura = idFactura;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
+	public void setArreglo(String arreglo) {
+		this.arreglo = arreglo;
+	}
+
+	public void setCostoAreglo(Double costoAreglo) {
+		this.costoAreglo = costoAreglo;
+	}
+
 	public void setCobrado(boolean cobrado) {
 		this.cobrado = cobrado;
 	}
-	
 }
