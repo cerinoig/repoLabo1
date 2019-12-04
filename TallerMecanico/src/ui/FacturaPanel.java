e7d61e671e1e088f2100cd78112f854f4a12f3dc
jesi <jesi@jesi-PC>
2019-12-03 22:25:53 -0300

    se ponen genericos los deshabilitar y los limpiarTextFields y los init

--- TallerMecanico/src/ui/FacturaPanel.java
+++ TallerMecanico/src/ui/FacturaPanel.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.awt.Color;
+
 import javax.swing.JCheckBox;
 import javax.swing.JTextField;
 
@@ -9,15 +11,41 @@ public abstract class FacturaPanel extends MiPanel {
 
 	private static final long serialVersionUID = 1L;
 	
-	
 	protected JTextField idFacturaTextField;
 	protected JTextField patenteTextField;
 	protected JTextField arregloTextField;
+	protected JTextField precioTextField;
 	protected JCheckBox cobradoCheckBox;
 
 	public FacturaPanel(Handler handler) {
 		super(handler);
 		
 	}
+	
+	@Override
+	public void initTextFields() {
+		idFacturaTextField = new JTextField();
+		patenteTextField = new JTextField();
+		arregloTextField = new JTextField();
+		precioTextField = new JTextField();
+	}
+	
+	
+	@Override
+	public void deshabilitarCampos() {
+		patenteTextField.setEditable(false);
+		patenteTextField.setBackground(Color.LIGHT_GRAY);
+		arregloTextField.setEditable(false);
+		arregloTextField.setBackground(Color.LIGHT_GRAY);
+		precioTextField.setEditable(false);
+		precioTextField.setBackground(Color.LIGHT_GRAY);
+	}
 
+	
+	@Override
+	public void limpiarCampos() {
+		patenteTextField.setText("");
+		arregloTextField.setText("");
+		precioTextField.setText("");
+	}
 }
