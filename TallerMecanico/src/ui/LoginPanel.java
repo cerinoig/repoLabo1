f45f43850ec0a50bbf2fe41e6cbe8614099f924d
MatiasManzano <matias.manzano89@gmail.com>
2019-12-08 09:42:21 -0300

    Se cambia el método

--- TallerMecanico/src/ui/LoginPanel.java
+++ TallerMecanico/src/ui/LoginPanel.java
@@ -70,7 +70,7 @@ public class LoginPanel extends JPanel {
 		login.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				handler.consultarUsuario(usuarioTextField.getText().toString().trim());
+				handler.login(usuarioTextField.getText().toString().trim());
 			}
 		});
 
