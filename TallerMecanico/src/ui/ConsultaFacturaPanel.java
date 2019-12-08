5b7117b2815f387a42583d4e332fa2e3104bfd74
MatiasManzano <matias.manzano89@gmail.com>
2019-12-08 19:17:03 -0300

    Se pasa el costo a BigDecimal y se pone la funcionaldad del boton en un metodo

--- TallerMecanico/src/ui/ConsultaFacturaPanel.java
+++ TallerMecanico/src/ui/ConsultaFacturaPanel.java
@@ -1,16 +1,15 @@
 package ui;
 
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
@@ -21,7 +20,7 @@ public class ConsultaFacturaPanel extends ConsultaPanel {
 
 	private static final long serialVersionUID = 1L;
 	FacturasTableModel facturasTableModel;
-	
+
 	public ConsultaFacturaPanel(Handler handler) {
 		super(handler);
 	}
@@ -32,31 +31,12 @@ public class ConsultaFacturaPanel extends ConsultaPanel {
 		confirmarBoton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				List<Factura> facturas = new ArrayList<>();
-				Double total = 0.00;				
-				for (int i = 0; i < facturasTableModel.getRowCount(); i++) {
-					Boolean checked = Boolean.valueOf(facturasTableModel.getValueAt(i, 4).toString());
-					if (checked) {
-						facturas.add(new Factura(Integer.valueOf(facturasTableModel.getValueAt(i, 0).toString()), 
-								facturasTableModel.getValueAt(i, 1).toString(), 
-								facturasTableModel.getValueAt(i, 2).toString(), 
-								Double.parseDouble(facturasTableModel.getValueAt(i, 3).toString()), checked));
-						Double subtotal = Double.parseDouble(facturasTableModel.getValueAt(i, 3).toString());
-						total = total + subtotal;
-					}
-				}
-				int input = JOptionPane.showOptionDialog(null, "Esta por pagar $" + total, "Cierre de caja",
-						JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
-
-				if (input == JOptionPane.OK_OPTION) {
-					handler.cobrarArreglo(facturas);
-					handler.mostrarExito("Listo! El pago se realizo con exito");
-				}
+				pagarFacturas(handler);
 			}
 		});
 
 	}
-	
+
 	@Override
 	public void initButtons() {
 		confirmarBoton = new JButton("Pagar");
@@ -84,6 +64,32 @@ public class ConsultaFacturaPanel extends ConsultaPanel {
 	@Override
 	public void initTableModel(Handler handler) {
 		facturasTableModel = new FacturasTableModel(handler.selectAllFacturas());
+	}
+
+	public void pagarFacturas(Handler handler) {
+		int input = 99999;
+		List<Factura> facturas = new ArrayList<>();
+		BigDecimal total = new BigDecimal(0);
+
+		for (int i = 0; i < facturasTableModel.getRowCount(); i++) {
+			Boolean checked = Boolean.valueOf(facturasTableModel.getValueAt(i, 4).toString());
+			if (checked) {
+				facturas.add(new Factura(Integer.valueOf(facturasTableModel.getValueAt(i, 0).toString()),
+						facturasTableModel.getValueAt(i, 1).toString(), facturasTableModel.getValueAt(i, 2).toString(),
+						new BigDecimal(facturasTableModel.getValueAt(i, 3).toString()), checked));
+
+				BigDecimal subtotal = new BigDecimal(facturasTableModel.getValueAt(i, 3).toString());
+				total = total.add(subtotal);
+			}
+		}
+
+		input = JOptionPane.showOptionDialog(null, "Esta por pagar $" + total.toString().replace(".", ","), "Cierre de caja",
+				JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+
+		if (input == JOptionPane.OK_OPTION) {
+			handler.cobrarArreglo(facturas);
+			handler.mostrarExito("Listo! El pago se realizo con exito");
+		}
 	}
 
 }
